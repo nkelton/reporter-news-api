12262ae901774243e1712dfac03ef3253b641c52
Nick Kelton <kelton.nicholas@gmail.com>
2019-12-12 00:09:41 -0600

    +remove unused code

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,4 @@
 var express = require("express");
 var router = express.Router();
-const muckrackService = require("../service/muckrack-service");
-
-/* GET stories. */
-// router.get("/stories", async function(req, res, next) {
-//   muckrackService.getStoriesFor(res, next, req.query.reporter);
-// });
 
 module.exports = router;
