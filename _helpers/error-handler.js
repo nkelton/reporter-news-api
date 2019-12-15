8376fbe3e168ffa40246055d5b193eeead9f6353
Nick Kelton <kelton.nicholas@gmail.com>
2019-12-15 01:54:15 -0600

    +Add custom error handling for 404 +Refactor app.js to catch jwt errors

--- _helpers/error-handler.js
+++ _helpers/error-handler.js
@@ -1,4 +1,4 @@
-module.exports = errorHandler;
+module.exports = { errorHandler, handle404 };
 
 function errorHandler(err, req, res, next) {
   if (typeof err === "string") {
@@ -18,4 +18,8 @@ function errorHandler(err, req, res, next) {
 
   // default to 500 server error
   return res.status(500).json({ message: err.message });
+}
+
+function handle404(req, res, next) {
+  return res.status(404).json({ message: "Invalid address" });
 }
--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var database = require("./_helpers/database");
 var jwt = require("./_helpers/jwt");
-var errorHandler = require("./_helpers/error-handler");
+const { errorHandler, handle404 } = require("./_helpers/error-handler");
 
 var usersRouter = require("./routes/users");
 var storiesRouter = require("./routes/stories");
@@ -28,26 +27,15 @@ app.use("/users", usersRouter);
 app.use("/stories", storiesRouter);
 app.use("/reporters", reportersRouter);
 
-//error handling
-app.use(errorHandler);
-
 //jwt authentication
 // app.use(jwt());
 
+//error handling
+app.use(errorHandler);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  return handle404(req, res, next);
 });
 
 module.exports = app;
